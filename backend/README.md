09aa5e9f3540b90f1fb4026976e83338a181c3f1
myusername <myemail>
2023-12-07 17:46:59 -0500

    Primer avance

---
+++ backend/README.md
@@ -0,0 +1,7 @@
+# Backend Hello
+## Recursos
+- https://www.npmjs.com/
+
+## Express
+- framework para Node.js
+- a
---
+++ backend/index.js
@@ -0,0 +1,29 @@
+//console.log('Hola Mundo')
+
+
+const express = require('express')
+
+const cors = require('cors')
+const server = express()
+
+server.use(cors());
+
+
+server.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+server.get('/ping', (req, res) =>{
+res.status(200).json(
+{
+
+    message: 'pong' 
+}
+)
+})
+
+
+
+
+
+server.listen(3100)
